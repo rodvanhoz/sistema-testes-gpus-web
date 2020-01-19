753000c55bb704cc50b7b97a807a24deb6e175d7
Rodrigo Vanhoz Ribeiro <rodrigo.vanhoz@@gmail.com>
2020-01-19 00:55:54 -0200

    first commit

---
+++ src/main/java/com/rvr/sistematestesgpus/SistemaTestesGpusWebApplication.java
@@ -0,0 +1,13 @@
+package com.rvr.sistematestesgpus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaTestesGpusWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaTestesGpusWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rvr/sistematestesgpus/SistemaTestesGpusWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rvr.sistematestesgpus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaTestesGpusWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
